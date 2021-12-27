3481bd0b340cafae153f07b3b8c451b0bdddc6ac
Gleb Sorkin <63267903+glebsorkin@users.noreply.github.com>
2021-12-27 23:09:09 +0300

    Add files via upload

---
+++ MPP 3/MPP_lab3/Mutex.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+
+namespace MPP_lab3
+{
+    public class Mutex
+    {
+        private int _usedBy;
+
+        public void Lock()
+        {
+            int id = Thread.CurrentThread.ManagedThreadId;
+            while (Interlocked.CompareExchange(ref _usedBy, id, 0) != 0)
+            {
+                Thread.Sleep(10);
+            }
+        }
+
+        public void Unlock()
+        {
+            int id = Thread.CurrentThread.ManagedThreadId;
+            Interlocked.CompareExchange(ref _usedBy, 0, id);
+        }
+    }
+}
---
+++ MPP 3/MPP_lab3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace MPP_lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex m = new Mutex();
+            for (int i = 0; i < 7; i++)
+            {
+                new Thread(() =>
+                    {
+                        m.Lock();
+                        Console.WriteLine("ID of thread that locked others: {0}",
+                            Thread.CurrentThread.ManagedThreadId);
+                        Thread.Sleep(1000);
+                        m.Unlock();
+                        Console.WriteLine("ID of thread that unlocked others: {0}",
+                            Thread.CurrentThread.ManagedThreadId);
+
+                    }
+                ).Start();
+            }
+        }
+    }
+}
